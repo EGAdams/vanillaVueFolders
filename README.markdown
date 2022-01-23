ae7994ee33beb945afd53de58fb2ba6c7ad9fa81
adamsl <eg1972@gmail.com>
2022-01-23 15:50:28 -0500

    first commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Vue Recursive Component Example
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/adamsl/pen/eYeOOqy](https://codepen.io/adamsl/pen/eYeOOqy).
+
+
---
+++ dist/script.js
@@ -0,0 +1,64 @@
+const root = {
+    text: 'Top Root Folder',
+    leaf: false,
+    expanded: true,
+    children: [{
+        text: 'Sub Folder 1',
+        leaf: false,
+        expanded: false,
+        children: [{
+            text: 'Sub Sub Folder 1',
+            leaf: false,
+            expanded: false,
+            children: [{
+                text: 'SomeFile1.js',
+                leaf: true
+            }]
+        }, {
+            text: 'Sub Sub Folder 2',
+            leaf: false,
+            expanded: false,
+            children: []
+        }, {
+            text: 'SomeFile.txt',
+            leaf: true
+        }]
+    }]
+};
+
+const folder = {
+    name: "folder",
+    template: '#folder-component',
+    props: {
+        folder: Object
+    },
+    methods: {
+        expand() {
+            if ( this.folder.leaf ) {
+                return;
+            }
+            this.folder.expanded = !this.folder.expanded;
+        }
+    }
+}
+
+const folders = {
+    name: 'root',
+    template: '#folders-component',
+    props: {
+        folder: Object
+    },
+    components: {
+        'folder': folder
+    }
+}
+
+new Vue( {
+    el: '.vue-app',
+    data: () => {
+        return root;
+    },
+    components: {
+        'root': folders
+    }
+});
